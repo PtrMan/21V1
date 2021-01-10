c41c531892f4ee73c07685fa2931474e430862c3
PtrMan <1067920+PtrMan@users.noreply.github.com>
2021-01-10 22:23:41 +0100

    update: better proposal generation, better motion

--- ProposalNnTrainer.py
+++ ProposalNnTrainer.py
@@ -44,28 +44,45 @@ def main():
     
     # list of tuple of input and target
     inputAndTarget = []
+
+
+    random.seed(42+7)
+
+    diffvecs = []
+    diffvecs.append([0.0,0.0,0.0]) # no motion
+    for iMagX in [0.0, 0.02, 0.05, 0.1]: # iterate over magnitude of motion
+        diffvecs.append([0.0,0.0,iMagX])
+        diffvecs.append([0.0,0.0,-iMagX])
+        diffvecs.append([0.0,iMagX,0.0])
+        diffvecs.append([0.0,-iMagX,0.0])
+        diffvecs.append([iMagX,0.0,0.0])
+        diffvecs.append([-iMagX,0.0,0.0])
+
+
+    for i in range(8):
+        x =random.uniform(-1.0, 1.0)
+        y =random.uniform(-1.0, 1.0)
+        z =random.uniform(-1.0, 1.0)
+
+        diffvecs.append([x*0.02,y*0.02,z*0.02])
+        diffvecs.append([x*0.06,y*0.06,z*0.06])
+    
     
 
     # training for moving object in center
     idx = -1
-    for iSceneDescMotion, iSceneDescriptionOutArr, lightBefore, lightAfter in [
-        ([0.0,0.0,0.0], [0.1, 0.9], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]), # no motion 
-        ([0.0,0.0,0.02], [0.9, 0.1], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]),  # slight motion forward
-        ([0.0,0.0,-0.02], [0.9, 0.1], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]),  # slight motion backward
-        ([0.0,0.0,0.06], [0.9, 0.1], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]),  # motion forward
-        ([0.0,0.0,-0.06], [0.9, 0.1], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]),  # motion backward
-
-        ([0.0,0.02,0.0], [0.9, 0.1], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]),
-        ([0.0,0.03,-0.02], [0.9, 0.1], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]),
-        ([0.0,-0.05,0.06], [0.9, 0.1], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]),
-        ([0.0,-0.08,-0.06], [0.9, 0.1], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]),
-
-        ([0.02,0.02,0.0], [0.9, 0.1], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]),
-        ([0.02,0.03,-0.02], [0.9, 0.1], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]),
-        ([0.02,-0.05,0.06], [0.9, 0.1], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]),
-        ([0.02,-0.08,-0.06], [0.9, 0.1], [0.0, 0.0, -3.0], [0.0, 0.0, -3.0]),
-        ]:
+    for iDiffvec in diffvecs:
         idx+=1
+
+        isAnyMotion = iDiffvec[0]*iDiffvec[0]+ iDiffvec[1]*iDiffvec[1]+ iDiffvec[2]*iDiffvec[2] > 0.001
+
+        iSceneDescriptionOutArr = [0.1,0.9] # no motion -> no proposal
+        if isAnyMotion:
+            iSceneDescriptionOutArr = [0.9,0.1]
+
+
+        lightBefore = [0.0, 0.0, -3.0]
+        lightAfter = [0.0, 0.0, -3.0]
 
         scene = Scene()
         scene.backgroundColor = [0.0, 1.0, 0.0] # green for better visualization
@@ -78,14 +95,14 @@ def main():
         scene.lightPos = lightBefore
 
         # render and read training images
-        imgBeforeGray = renderSceneAndReturnImageGray64(scene, f'trainMotion{idx}A.png')
+        imgCurrentGray = renderSceneAndReturnImageGray64(scene, f'trainMotion{idx}B.png')
 
         # move camera to get movement vector
-        scene.cameraPos[0] += iSceneDescMotion[0]
-        scene.cameraPos[1] += iSceneDescMotion[1]
-        scene.cameraPos[2] += iSceneDescMotion[2]
+        scene.cameraPos[0] -= iDiffvec[0]
+        scene.cameraPos[1] -= iDiffvec[1]
+        scene.cameraPos[2] -= iDiffvec[2]
 
-        imgCurrentGray = renderSceneAndReturnImageGray64(scene, f'trainMotion{idx}B.png')
+        imgBeforeGray = renderSceneAndReturnImageGray64(scene, f'trainMotion{idx}A.png')
 
         flow = cv2.calcOpticalFlowFarneback(imgBeforeGray,imgCurrentGray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
         flowArr = flow.flatten() # convert image array to flat array
@@ -192,8 +209,6 @@ def main():
 
     while True: # "game"loop
         itCnt+=1
-
-        # TODO< add code for supervised NN and training and storage of NN params! >
 
         if True: # codeblock
             optimizer.zero_grad()   # zero the gradient buffers
