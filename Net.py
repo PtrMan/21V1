7da19beb049c44a26a9df14964143e2b00b7c9ac
PtrMan <1067920+PtrMan@users.noreply.github.com>
2021-01-10 21:17:49 +0100

    update: playing around with relu

--- Net.py
+++ Net.py
@@ -3,15 +3,17 @@
 import torch.nn as nn
 
 class Net(nn.Module):
-	def __init__(self, nInput, nOutput):
-		super(Net, self).__init__()
-		nHidden = 1 # number of hidden neurons
-		self.fca1 = nn.Linear(nInput, nHidden)
-		self.fca2 = nn.Linear(nHidden, nOutput)
-		#self.res = nn.Tanh()
+    def __init__(self, nInput, nOutput):
+        super(Net, self).__init__()
+        nHidden = 3 # number of hidden neurons
+        self.fca1 = nn.Linear(nInput, nHidden)
+        self.a = nn.ReLU()
+        self.fca2 = nn.Linear(nHidden, nOutput)
+        #self.b = nn.Tanh()
 
-	def forward(self, x):
-		x = self.fca1(x)
-		x = self.fca2(x)
-		#x = self.res(x)
-		return x
+    def forward(self, x):
+        x = self.fca1(x)
+        x = self.a(x)
+        x = self.fca2(x)
+        #x = self.b(x)
+        return x
