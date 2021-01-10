d922eee92c9f1e10367d850a22a595361cee49db
PtrMan <1067920+PtrMan@users.noreply.github.com>
2021-01-10 23:53:43 +0100

    update: net

--- Net.py
+++ Net.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class Net(nn.Module):
     def __init__(self, nInput, nOutput):
         super(Net, self).__init__()
-        nHidden = 3 # number of hidden neurons
+        nHidden = 1 # number of hidden neurons
         self.fca1 = nn.Linear(nInput, nHidden)
         self.a = nn.ReLU()
         self.fca2 = nn.Linear(nHidden, nOutput)
@@ -13,7 +13,7 @@ class Net(nn.Module):
 
     def forward(self, x):
         x = self.fca1(x)
-        x = self.a(x)
+        #x = self.a(x)
         x = self.fca2(x)
         #x = self.b(x)
         return x
